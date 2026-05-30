8171e359282aba0c618661175bca85ff70c3c4b2
rajni77289 <rajni575700@gmail.com>
2026-05-30 15:55:14 +0530

    jwt

--- src/Components/API/Api.js
+++ src/Components/API/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = "http://localhost:8000";
-const API_URL = "https://nspl-backend.vercel.app/";
+const API_URL = "http://localhost:8000";
+// const API_URL = "https://nspl-backend.vercel.app/";
 
 function getToken(){
     const data=localStorage && localStorage.getItem("tokenAuth");
